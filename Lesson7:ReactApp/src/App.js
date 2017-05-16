b8c59530ac128bd042d407af96b8ad5035ae90ea
ximet <dq.ximet@gmail.com>
2017-05-16 21:10:06 +0300

    step 2(ReactApp)

---
+++ Lesson7:ReactApp/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
+import { AppView } from './views/AppView.js';
+
+
+ReactDOM.render(
+            <AppView />,
+
+        window.document.getElementById('container')
+    );
